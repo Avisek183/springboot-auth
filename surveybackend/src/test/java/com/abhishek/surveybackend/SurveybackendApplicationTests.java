b28b800d1d4d7a8f9b3de8054630081c96de08a9
Abhishek612 <abhishek.kumar3@clarivate.com>
2021-04-22 17:34:05 +0530

    Initial Commit

---
+++ surveybackend/src/test/java/com/abhishek/surveybackend/SurveybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.surveybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
