8a6dfdbc0de32681d432dc4925c5a3abb5bc97fe
Abhishek612 <abhishek.kumar3@clarivate.com>
2021-04-24 16:45:12 +0530

    Backend overall

---
+++ src/main/java/com/abhishek/surveybackend/survey/controllers/SurveydetailsController.java
@@ -0,0 +1,54 @@
+package com.abhishek.surveybackend.survey.controllers;
+import com.abhishek.surveybackend.survey.models.Surveydetails;
+import com.abhishek.surveybackend.survey.service.SurveydetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+
+@RestController
+@RequestMapping("/api/surveytable")
+public class SurveydetailsController {
+    @Autowired
+    SurveydetailsService surveydetailsService;
+
+
+    @GetMapping("")
+    public List<Surveydetails> list() {
+        return surveydetailsService.listAllUser();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Surveydetails> get(@PathVariable Integer id) {
+        try {
+            Surveydetails surveydetails = surveydetailsService.getUser(id);
+            return new ResponseEntity<Surveydetails>(surveydetails, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<Surveydetails>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @PostMapping("/add")
+    public void add(@RequestBody Surveydetails surveydetails) {
+        surveydetailsService.saveUser(surveydetails);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@RequestBody Surveydetails surveydetails, @PathVariable Integer id) {
+        try {
+            Surveydetails existUser = surveydetailsService.getUser(id);
+            surveydetails.setId(id);
+            surveydetailsService.saveUser(surveydetails);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+
+        surveydetailsService.deleteUser(id);
+    }
+}
